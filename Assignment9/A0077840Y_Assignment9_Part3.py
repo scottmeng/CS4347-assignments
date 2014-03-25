ae35e48be15bae8366036f1b37c84651b42b97d5
scottmeng <blackridermkz@gmail.com>
2014-03-25 14:20:02 +0800

    init commit for part 3

--- Assignment9/A0077840Y_Assignment9_Part3.py
+++ Assignment9/A0077840Y_Assignment9_Part3.py
@@ -7,10 +7,19 @@ from scipy.io.wavfile import read, write
 import os
 os.getcwd()                                              # get the directory of current process
 
-input_dir = "original.wav"
+lower_bound = 200
+upper_bound = 1000
 
-def time_to_freq(data, window):
+input_dir = "original.wav"
+output_dir = "reconstructed.wav"
+
+def time_to_freq(data, window, bins_to_freq):
+    lower_index = int(lower_bound / bins_to_freq)
+    upper_index = int(upper_bound / bins_to_freq)
     fft = numpy.fft.fft(data * window)
+    fft[0: lower_index] = numpy.zeros(lower_index)
+    fft[upper_index: 512 - upper_index] = numpy.zeros(512 - 2 * upper_index)
+    fft[512 - lower_index: 512] = numpy.zeros(lower_index)
     return fft
 
 def sine_window(length):
@@ -27,20 +36,17 @@ data = original / 32768.
 
 num_buffers = len(data) / 256 - 1
 reconstructed = numpy.zeros(len(data))
+bins_to_freq = sample_freq / 512.
 
 for i in range(num_buffers):
     start = int(i * 256)
     end = start + 512
-    freq_buffer = time_to_freq(data[start: end], sine_window(512))
+    freq_buffer = time_to_freq(data[start: end], sine_window(512), bins_to_freq)
     reconstructed[start: end] += freq_to_time(freq_buffer, sine_window(512))
 
 reconstructed = numpy.array(reconstructed * 32768, dtype = numpy.int16)       # convert data into 16-bit integer
 
-residual = original - reconstructed
-residual = residual[256: len(residual) - 256]
+write(output_dir, sample_freq, reconstructed)
 
-residual_error = max(abs(residual))
-
-print residual_error
 
  
