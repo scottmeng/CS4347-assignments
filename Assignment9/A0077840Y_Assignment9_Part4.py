d903c31beef64498644ca84a78f6d856a918686c
scottmeng <blackridermkz@gmail.com>
2014-03-25 18:56:18 +0800

    changes on quantization

--- Assignment9/A0077840Y_Assignment9_Part4.py
+++ Assignment9/A0077840Y_Assignment9_Part4.py
@@ -7,8 +7,9 @@ from scipy.io.wavfile import read, write
 import os
 os.getcwd()                                              # get the directory of current process
 
-lower_bound = 200
-upper_bound = 3000
+lower_bound = 500
+upper_bound = 1500
+quan_bits = 4
 
 input_dir = "original.wav"
 output_dir = "reconstructed_part4.wav"
@@ -20,9 +21,11 @@ def time_to_freq(data, window, bins_to_freq):
     fft[0: lower_index] = numpy.zeros(lower_index)
     fft[upper_index: 512 - upper_index] = numpy.zeros(512 - 2 * upper_index)
     fft[512 - lower_index: 512] = numpy.zeros(lower_index)
-    fft.real = numpy.array(fft.real * 32767, dtype = numpy.int16)
-    fft.imag = numpy.array(fft.imag * 32767, dtype = numpy.int16)
-
+    print max(fft.real)
+    real_parts = numpy.array(fft.real * (2 ** quan_bits), dtype = numpy.int32)
+    fft.real = real_parts / float(2 ** quan_bits)
+    imag_parts = numpy.array(fft.imag * (2 ** quan_bits), dtype = numpy.int32)
+    fft.imag = imag_parts / float(2 ** quan_bits)
     return fft
 
 def sine_window(length):
@@ -37,6 +40,9 @@ sample_freq, original = read(input_dir)
 
 data = original / 32768.
 
+print max(data)
+exit(0)
+
 num_buffers = len(data) / 256 - 1
 reconstructed = numpy.zeros(len(data))
 bins_to_freq = sample_freq / 512.
@@ -47,7 +53,7 @@ for i in range(num_buffers):
     freq_buffer = time_to_freq(data[start: end], sine_window(512), bins_to_freq)
     reconstructed[start: end] += freq_to_time(freq_buffer, sine_window(512))
 
-reconstructed = numpy.array(reconstructed, dtype = numpy.int16)       # convert data into 16-bit integer
+reconstructed = numpy.array(reconstructed * 32768, dtype = numpy.int16)       # convert data into 16-bit integer
 
 write(output_dir, sample_freq, reconstructed)
 
