e29af644972441413540d6617b85c65e8a7547e4
scottmeng <blackridermkz@gmail.com>
2014-04-05 22:51:43 +0800

    add J48 classifier

--- Assignment10/A0077840Y_Assignment10_classifier.py
+++ Assignment10/A0077840Y_Assignment10_classifier.py
@@ -8,7 +8,7 @@ import os
 os.getcwd()                                              # get the directory of current process
 
 input_dir = "../music_speech.mf"
-new_input_dir = "../expanded-music-speech.mf"
+new_data_dir = "../expanded-music-speech.mf"
 output_dir_regular = "Assignment10_regular_new.arff"
 output_dir_normalized = "Assignment10_normalized_new.arff"
 num_windows = 26
@@ -92,51 +92,6 @@ def J48_classifier(data):
         else:
             return "music"
 
-input_file = open(input_dir)                                    # read from ground truth file
-output_file_regular = open(output_dir_regular, 'w')             # write into output ARFF file
-output_file_normalized = open(output_dir_normalized, 'w')
-output_arff_title(output_file_regular)                          # output titles to ARFF files
-output_arff_title(output_file_normalized)
-
-file_names = input_file.readlines()                             # read in all file names
-arff_results = numpy.empty((len(file_names), num_windows * 2))
-labels = list()                                                 # list to store all the labels
-
-for j in range(len(file_names)):
-    music_dir, label = file_names[j].split()                    # extract music and speech file directory
-    labels.append(label)
-    samp_rate, data = read("../" + music_dir)
-
-    mel_interval = cal_mel_interval(samp_rate)
-    bin_val = samp_rate / 1024.0
-
-    windows = get_window(num_windows)
-
-    data = apply_pre_emphasis(data / 32768.)                    # normalize wav file data
-
-    num_buffers = len(data) / 512 - 1                           # calculate the number of buffer slices
-    buffers = numpy.zeros((513, num_buffers))                   # create matrix to store buffer data
-        
-    for i in range(num_buffers):                                # put buffer slices in matrix
-        start = i * 512
-        end = start + 1024
-        buffers[:,i] = fft_spectrum(data[start:end], numpy.hamming(1024))
-
-    result = numpy.dot(windows, buffers)                        # apply 26 windows in one go
-    result = numpy.log10(result)                                # apply base 10 log
-    result = scipy.fftpack.dct(result, axis = 0)                # apply dct across 26 MFCC values
-
-    arff_results[j] = get_mean_and_std(result)
-
-output_arff_data(arff_results, output_file_regular)
-arff_results_normalized = normalize_data(arff_results)
-output_arff_data(arff_results_normalized, output_file_normalized)
-
-output_file_regular.close()
-output_file_normalized.close()  
-
-
-# start of classifying process
 data_file = open(output_dir_normalized)
 arff_entries = data_file.readlines()
 start = False
@@ -151,8 +106,8 @@ for arff_entry in arff_entries:
         if classified_class == original_class:
             num_correct_entries += 1
         num_entries += 1
-    if arff_entry == "@DATA\n":                                 # start reading data when @DATA tag is encountered
+    if arff_entry == "@DATA\n":
         start = True
 
-print "Accuracy:" + str(num_correct_entries / float(num_entries))
-
+print num_entries
+print num_correct_entries
